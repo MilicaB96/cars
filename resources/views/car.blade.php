0f91b54c03e9cb5cfeb13769e5ab08b7ed324f34
Milica <magiflowerchild@gmial.com>
2022-02-10 20:11:59 +0100

    Code formatting

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
-@section('title',$car->title)
+@section('title', $car->title)
 
 @section('content')
-        <h1>{{$car->title}}</h1>
-        <p>Produced by {{$car->producer}}</p>
-        <p>Door number is {{$car->number_of_doors}}</p>
+    <h1>{{ $car->title }}</h1>
+    <p>Produced by {{ $car->producer }}</p>
+    <p>Door number is {{ $car->number_of_doors }}</p>
 @endsection
--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
-@section('title','Cars')
+@section('title', 'Cars')
 @section('content')
-<ul class="list-group list-group-flush">
-            @foreach ($cars as $car)
-               <a class="text-decoration-none" href="/cars/{{$car->id}}"> <li class="list-group-item" >{{$car->title}}</li></a>
-            @endforeach
-        </ul>
-@endsection 
+    <ul class="list-group list-group-flush">
+        @foreach ($cars as $car)
+            <li class="list-group-item">
+                <a class="text-decoration-none" href="/cars/{{ $car->id }}"> {{ $car->title }}</a>
+            </li>
+        @endforeach
+    </ul>
+@endsection
