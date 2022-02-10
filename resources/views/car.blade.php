553892d9fc680354250287999f14e9874c9afebf
Milica <magiflowerchild@gmial.com>
2022-02-10 19:43:18 +0100

    added layout and change cars, car, about blade accordingly

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div>
-        <h1>{{$car->title}}</h1>
+@extends('layouts.app')
+@section('title',$car->title)
+
+@section('content')
+<h1>{{$car->title}}</h1>
         <p>Produced by {{$car->producer}}
         </p>
         <p>Door number is {{$car->number_of_doors}}</p>
-    </div>
-</body>
-</html>
+@endsection
--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div>
-        <ul>
-            <h1>Cars:</h1>
+@extends('layouts.app')
+@section('title','Cars')
+@section('content')
+<ul>
             @foreach ($cars as $car)
-               <a href="/cars/{{$car->id}}"> <li>{{$car->title}}</li></a>
+               <a style="color:black;" href="/cars/{{$car->id}}"> <li>{{$car->title}}</li></a>
             @endforeach
         </ul>
-    </div>
-</body>
-</html>
+@endsection 
