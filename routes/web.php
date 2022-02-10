fab75a60d88ccb5a5215642fb3ed38f4e22cfc67
Milica <magiflowerchild@gmial.com>
2022-02-10 18:27:44 +0100

    added about and midified welcome

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\RiskyTestError;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/about', function () {
+    return view('about');
 });
